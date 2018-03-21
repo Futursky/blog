b1db43a8541a1c18f24e8318d29ddb96cf3d7f67
Futursky <futurius@gmail.com>
2018-03-21 15:45:54 +0200

    setup cors and api url

--- app-frontend/src/app/app.component.ts
+++ app-frontend/src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { environment } from '../environments/environment';
 
 @Component({
   selector: 'app-root',
@@ -12,7 +13,7 @@ export class AppComponent {
   posts;
 
   constructor(private http:HttpClient){
-    this.http.get('http://localhost:3000/posts.json')
+    this.http.get(environment.apiUrl + '/posts.json')
       .subscribe(res => this.posts = res);
   }
 }
