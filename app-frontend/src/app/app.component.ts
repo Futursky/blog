3123f9465bbca715a0b6f253ef47e3ab839c0513
Futursky <futurius@gmail.com>
2018-03-21 12:54:56 +0200

    fix frontend

--- app-frontend/src/app/app.component.ts
+++ app-frontend/src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  providers: [HttpClient]
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app works!';
+  posts;
+
+  constructor(private http:HttpClient){
+    this.http.get('http://localhost:3000/posts.json')
+      .subscribe(res => this.posts = res);
+  }
 }
